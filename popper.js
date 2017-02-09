1be0aa3374807ece77e0b0082cc3a5239da9070d
christopherhill <thorsdaddy77@gmail.com>
2017-02-09 11:49:08 -0600

    cleanup

--- popper.js
+++ popper.js
@@ -1,1 +1,13 @@
-console.log('hello');
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('getYouTube');
+    checkPageButton.addEventListener('click', function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            d = document;
+            f = d.createElement('form');
+            f.action = 'https://www.youtube.com/';
+            f.method = 'post';
+            d.body.appendChild(f);
+            f.submit();
+        });
+    });
+});
